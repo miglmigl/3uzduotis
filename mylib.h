d0e8bf386cc508f62911ce12f5eeb2132ed007ef
miglmigl <144427943+miglmigl@users.noreply.github.com>
2023-10-12 10:55:44 +0300

    #14
    Sukurta struktūra studentas_padalinimui, su kuria veikia failo kūrimo ir spauzdinimo funkcijos.

--- mylib.h
+++ mylib.h
@@ -25,11 +25,20 @@ struct studentas
         float med;
     };
 
+struct studentas_padalinimui
+{
+    string var, pav;
+    vector<int> paz;
+    int egz;
+    float vid;
+};
+
+
 vector<studentas> ivesk(int stud_sk, string gen);
 void isvedimas(vector<studentas> &grupe, string gen, string ats);
 void pal_pav(vector<studentas> &grupe);
 void gen_failas(int stud_gen_sk, int stud_gen_nd);
-void padalinto_sapuzdinimas(vector<int>& studentai, string& failo_pav);
+void padalinto_sapuzdinimas(vector<studentas_padalinimui>& studentai, string& failo_pav);
 
 #endif //MYLIB_H_INCLUDED
 
