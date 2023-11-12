fff5719b161c7faecfb501990b40b9abe8f3e6b2
miglmigl <144427943+miglmigl@users.noreply.github.com>
2023-11-12 23:27:26 +0200

    #26

--- main.cpp
+++ main.cpp
@@ -89,14 +89,14 @@ int main() {
                         nuskaitymas(failas, grupe);
                         auto Nuskaitymo_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Nuskaitymo_laikas = Nuskaitymo_end - Nuskaitymo_start;
-                        //cout << kiekis << " irasu nuskaitymas uztruko: " << Nuskaitymo_laikas.count() << " s;" << endl;
+                        cout << kiekis << " irasu nuskaitymas uztruko: " << Nuskaitymo_laikas.count() << " s;" << endl;
 
                         
                         auto Rusiavimas_start = chrono::high_resolution_clock::now();
                         pal_pav(grupe, rus_index);
                         auto Rusiavimas_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Rusiavimas_laikas = Rusiavimas_end - Rusiavimas_start;
-                        //cout << kiekis << " irasu rusiavimas pagal " << rus_index << " uztruko: " << Rusiavimas_laikas.count() << " s;" << endl;
+                        cout << kiekis << " irasu rusiavimas pagal " << rus_index << " uztruko: " << Rusiavimas_laikas.count() << " s;" << endl;
                         
 
                         auto Padalinimo_start = chrono::high_resolution_clock::now();
@@ -106,7 +106,7 @@ int main() {
                         kietiakai = grupe;
                         auto Padalinimo_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Padalinimo_laikas = Padalinimo_end - Padalinimo_start;
-                        cout <</* kiekis << " irasu surusiavimas i dvi grupes uztruko: " << */Padalinimo_laikas.count() /* << " s;" */ << endl;
+                        cout <<kiekis << " irasu surusiavimas i dvi grupes uztruko: " << Padalinimo_laikas.count() << " s;"  << endl;
 
 
                         pal_pav(kietiakai, rus_index);
@@ -117,17 +117,16 @@ int main() {
                         padalinto_sapuzdinimas(kietiakai, "kietiakai.txt");
                         auto Kietiaku_sp_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Kietiaku_sp_laikas = Kietiaku_sp_end - Kietiaku_sp_start;
-                        //cout << kiekis << " irasu kietiaku irasymas i faila uztruko: " << Kietiaku_sp_laikas.count() << " s;" << endl;
+                        cout << kiekis << " irasu kietiaku irasymas i faila uztruko: " << Kietiaku_sp_laikas.count() << " s;" << endl;
 
 
                         auto Vargsiuku_sp_start = chrono::high_resolution_clock::now();
                         padalinto_sapuzdinimas(vargsiukai, "vargsiukai.txt");
                         auto Vargsiuku_sp_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Vargsiuku_sp_laikas = Vargsiuku_sp_end - Vargsiuku_sp_start;
-                        //cout << kiekis << " irasu vargsiuku irasymas i faila uztruko: " << Vargsiuku_sp_laikas.count() << " s;" << endl;
-
-                        //double visas_laikas = Nuskaitymo_laikas.count() + Rusiavimas_laikas.count() + Padalinimo_laikas.count() + Kietiaku_sp_laikas.count() + Vargsiuku_sp_laikas.count();
-                        //cout << kiekis << " irasu testo laikas: " << visas_laikas << " s;" << endl;
+                        cout << kiekis << " irasu vargsiuku irasymas i faila uztruko: " << Vargsiuku_sp_laikas.count() << " s;" << endl;
+                        double visas_laikas = Nuskaitymo_laikas.count() + Rusiavimas_laikas.count() + Padalinimo_laikas.count() + Kietiaku_sp_laikas.count() + Vargsiuku_sp_laikas.count();
+                        cout << kiekis << " irasu testo laikas: " << visas_laikas << " s;" << endl;
 
                         grupe.clear();
 
@@ -232,14 +231,14 @@ int main() {
                         nuskaitymas(failas, grupe);
                         auto Nuskaitymo_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Nuskaitymo_laikas = Nuskaitymo_end - Nuskaitymo_start;
-                        //cout << kiekis << " irasu nuskaitymas uztruko: " << Nuskaitymo_laikas.count() << " s;" << endl;
+                        cout << kiekis << " irasu nuskaitymas uztruko: " << Nuskaitymo_laikas.count() << " s;" << endl;
 
 
                         auto Rusiavimas_start = chrono::high_resolution_clock::now();
                         pal_pav_list(grupe, rus_index);
                         auto Rusiavimas_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Rusiavimas_laikas = Rusiavimas_end - Rusiavimas_start;
-                        //cout << kiekis << " irasu rusiavimas pagal " << rus_index << " uztruko: " << Rusiavimas_laikas.count() << " s;" << endl;
+                        cout << kiekis << " irasu rusiavimas pagal " << rus_index << " uztruko: " << Rusiavimas_laikas.count() << " s;" << endl;
                         
 
                         
@@ -250,7 +249,7 @@ int main() {
                         kietiakai = grupe;
                         auto Padalinimo_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Padalinimo_laikas = Padalinimo_end - Padalinimo_start;
-                        cout <</* kiekis << " irasu surusiavimas i dvi grupes uztruko: " << */Padalinimo_laikas.count() /* << " s;" */ << endl;
+                        cout << kiekis << " irasu surusiavimas i dvi grupes uztruko: " << Padalinimo_laikas.count()  << " s;"  << endl;
 
                         pal_pav_list(kietiakai, rus_index);
                         pal_pav_list(vargsiukai, rus_index);
@@ -259,17 +258,16 @@ int main() {
                         padalinto_sapuzdinimas(kietiakai, "kietiakai.txt");
                         auto Kietiaku_sp_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Kietiaku_sp_laikas = Kietiaku_sp_end - Kietiaku_sp_start;
-                        //cout << kiekis << " irasu kietiaku irasymas i faila uztruko: " << Kietiaku_sp_laikas.count() << " s;" << endl;
+                        cout << kiekis << " irasu kietiaku irasymas i faila uztruko: " << Kietiaku_sp_laikas.count() << " s;" << endl;
 
 
                         auto Vargsiuku_sp_start = chrono::high_resolution_clock::now();
                         padalinto_sapuzdinimas(vargsiukai, "vargsiukai.txt");
                         auto Vargsiuku_sp_end = chrono::high_resolution_clock::now();
                         chrono::duration<double> Vargsiuku_sp_laikas = Vargsiuku_sp_end - Vargsiuku_sp_start;
-                        //cout << kiekis << " irasu vargsiuku irasymas i faila uztruko: " << Vargsiuku_sp_laikas.count() << " s;" << endl;
-
+                        cout << kiekis << " irasu vargsiuku irasymas i faila uztruko: " << Vargsiuku_sp_laikas.count() << " s;" << endl;
                         double visas_laikas = Nuskaitymo_laikas.count() + Rusiavimas_laikas.count() + Padalinimo_laikas.count()  + Kietiaku_sp_laikas.count() + Vargsiuku_sp_laikas.count();
-                        //cout << kiekis << " irasu testo laikas: " << visas_laikas << " s;" << endl;
+                        cout << kiekis << " irasu testo laikas: " << visas_laikas << " s;" << endl;
 
                         grupe.clear();
 
